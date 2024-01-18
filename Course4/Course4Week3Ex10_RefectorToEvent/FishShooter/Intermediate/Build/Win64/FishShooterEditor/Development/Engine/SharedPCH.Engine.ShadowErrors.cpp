add492cef2a9dced3d8532de1bdf332ea97031f5
Colognesi <andres.e.colognesi@accenture.com>
2024-01-18 11:12:08 -0300

    feat: Add gitignore file and convert EventRefactor project to UE5.3

--- Course4/Course4Week3Ex10_RefectorToEvent/FishShooter/Intermediate/Build/Win64/FishShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30038
-#include "C:/Users/timch/Documents/Unreal Projects/FishShooter/Intermediate/Build/Win64/FishShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
